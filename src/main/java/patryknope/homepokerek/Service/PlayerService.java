06e2f13db1b0ddea4907d478d41868aaf31bd918
patryk <patrykoloss@gmail.com>
2022-12-18 19:04:01 +0100

    Add PlayerService

---
+++ src/main/java/patryknope/homepokerek/Service/PlayerService.java
@@ -0,0 +1,66 @@
+package patryknope.homepokerek.Service;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import patryknope.homepokerek.Model.Game;
+import patryknope.homepokerek.Model.Player;
+import patryknope.homepokerek.Repository.PlayerRepository;
+
+import java.util.List;
+import java.util.Set;
+
+
+@Service
+public class PlayerService {
+
+    @Autowired
+    PlayerRepository playerRepository;
+
+    public Player findPlayerById(Long id) {
+        return playerRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
+    }
+
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAll();
+    }
+
+    public Player addPlayer(Player player) {
+        if (canPlayerBeDeleted(player)) {
+            return playerRepository.save(player);
+        } else {
+            return null; //TODO: throw exception
+        }
+
+    }
+
+    public void deletePlayerById(Long id) {
+        var playerToDelete = findPlayerById(id);
+        playerRepository.deleteById(playerToDelete.getId());
+    }
+
+    public Player updatePlayer(Player player) {
+        var playerToUpdate = findPlayerById(player.getId());
+        playerToUpdate.setFirstName(player.getFirstName());
+        playerToUpdate.setLastName(player.getFirstName());
+        playerToUpdate.setEmail(player.getEmail());
+        playerToUpdate.setTelephoneNumber(player.getTelephoneNumber());
+        return playerRepository.save(playerToUpdate);
+    }
+
+    public Set<Game> getGamesOfPlayer(Long playerId) {
+        var player = findPlayerById(playerId);
+        if (player.getGameSet().size() == 0) {
+            return Set.of();
+        } else {
+            return player.getGameSet();
+        }
+
+    }
+
+    //helper function to determine if player can be deleted
+    private boolean canPlayerBeDeleted(Player player) {
+        return !playerRepository.existsByEmail(player.getEmail()) &&
+                !playerRepository.existsByTelephoneNumber(player.getTelephoneNumber());
+    }
+}
