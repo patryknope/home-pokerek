7718352ab9467cf2550ce5c96dec230c3f4c23e1
patryk <patrykoloss@gmail.com>
2022-12-18 18:27:23 +0100

    Project setup

--- src/main/java/patryknope/homepokerek/HomepokerekApplication.java
+++ src/main/java/patryknope/homepokerek/HomepokerekApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomepokerekApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomepokerekApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HomepokerekApplication.class, args);
+    }
 
 }
--- src/test/java/patryknope/homepokerek/HomepokerekApplicationTests.java
+++ src/test/java/patryknope/homepokerek/HomepokerekApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HomepokerekApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
