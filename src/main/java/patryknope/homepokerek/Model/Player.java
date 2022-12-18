b07eb8025f4c9ccb389e7f5c1fe93dfc60385a24
patryk <patrykoloss@gmail.com>
2022-12-18 18:26:32 +0100

    Add player model

---
+++ src/main/java/patryknope/homepokerek/Model/Player.java
@@ -0,0 +1,34 @@
+package patryknope.homepokerek.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @Email
+    private String email;
+    @Pattern(regexp = "[1-9]\\d{8}") // must be a 9 digit number not starting with 0
+    private String telephoneNumber;
+    private int gamesWon;
+
+}
