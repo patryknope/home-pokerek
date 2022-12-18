ea222917c1555d60fec49c5b6a5b6e9796ee3822
patryk <patrykoloss@gmail.com>
2022-12-18 18:19:48 +0100

    Initial commit

---
+++ src/main/java/patryknope/homepokerek/HomepokerekApplication.java
@@ -0,0 +1,13 @@
+package patryknope.homepokerek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepokerekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomepokerekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/patryknope/homepokerek/HomepokerekApplicationTests.java
@@ -0,0 +1,13 @@
+package patryknope.homepokerek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepokerekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
